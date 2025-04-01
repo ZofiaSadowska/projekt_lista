8de6249c5feb31609d7bae29efd253f3edd27179
ZofiaSadowska <113286218+ZofiaSadowska@users.noreply.github.com>
2025-04-01 15:29:15 +0200

    Poczatek programu

---
+++ projekt/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace projekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dodaj_Click(object sender, RoutedEventArgs e)
+        {
+
+            PopUp nowaLista = new PopUp();
+
+            nowaLista.ShowDialog();
+            
+            
+            ///var rnd = new Random();
+
+            ///lista.Items.Add(new { m_nID = rnd.Next(), m_strName = "Zosia" });
+
+        }
+    }
+}
